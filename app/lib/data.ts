09df0da01a293fe8cf332784c8f7f718c2e95f01
Miquel  Aubà <miquel.auba@adevinta.com>
2025-08-13 11:26:58 +0200

    Implement loading states and refactor dashboard components; add data fetching for cards, revenue chart, and latest invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,11 +17,14 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // const delayMiliseconds = 3000; // 3 seconds
+    // await new Promise((resolve) => setTimeout(resolve, delayMiliseconds));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    // console.log(
+    //   `Data fetch completed after ${delayMiliseconds / 1000} seconds.`
+    // );
 
     return data;
   } catch (error) {
